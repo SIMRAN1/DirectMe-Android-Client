fa3e418836b3e306dc69aaf6585477c6a2a65c37
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2017-02-20 04:03:30 +0530

    variables changed, sound solved

--- app/src/main/java/com/silab/direct_me/Config.java
+++
@@ -1,9 +0,0 @@
-package com.silab.direct_me;
-
-
-public class Config {
-    public static final String COMODITY_URL = "";
-    public static final String PARKED_URL = "";
-    public static final String PARKING_URL = "";
-    public static final String GARAGE_URL = "";
-}
